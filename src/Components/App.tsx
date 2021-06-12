3e291088db9c44a2994d7c117f2eda85a867e30f
mantonoiu <antonoiumadalin@gmail.com>
2021-06-12 10:43:56 +0100

    cra-ts-sass-eslint-prettier-mui

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -1,8 +1,27 @@
+import './App.scss';
 import React from 'react';
+import Button from '@material-ui/core/Button';
+import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
+import { Grid } from '@material-ui/core';
 
 class App extends React.PureComponent {
     render(): JSX.Element {
-        return <div>App</div>;
+        return (
+            <Grid
+                container
+                spacing={0}
+                direction="column"
+                alignItems="center"
+                justify="center"
+                style={{ minHeight: '100vh' }}
+            >
+                <Grid item xs={3}>
+                    <Button variant="contained" color="primary" endIcon={<InsertEmoticonIcon />}>
+                        Hello World
+                    </Button>
+                </Grid>
+            </Grid>
+        );
     }
 }
 
